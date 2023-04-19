d9bade88afe85102fb39dda0dcd7d94746e9b0ef
Guoqiang QI <guoqiang.qi1@gmail.com>
2023-04-19 09:08:38 +0000

    Set specific delimiters for streaming data.

--- src/stream-test.go
+++ src/stream-test.go
@@ -92,15 +92,15 @@ func main() {
 
 	reader := bufio.NewReader(resp.Body)
 	for {
-		line, err := reader.ReadString('\n')
+		line, err := reader.ReadString('\u00A9')
 		if err != nil {
 			fmt.Println("Finish stream.")
 			return
 		}
-		line = strings.TrimSuffix(line, "\n")
+		line = strings.TrimSuffix(line, "\u00A9")
 		if strings.HasPrefix(line, "data:") {
-			data := strings.TrimSpace(strings.TrimPrefix(line, "data:"))
-			fmt.Println(data)
+			data := strings.TrimPrefix(line, "data:")
+			fmt.Print(data)
 		}
 	}
 
