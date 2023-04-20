88d7810f57f9b04f856ba15a67608803102984f9
Guoqiang QI <guoqiang.qi1@gmail.com>
2023-04-20 03:29:05 +0000

    Refactor the ExponentialBackOff method to optimize the sleep task setting.

--- utils/exponential_backoff.go
+++ utils/exponential_backoff.go
@@ -37,22 +37,20 @@ func (ccsf *ChatCompletionStreamFunc) DoChatWork(messages []openai.ChatCompletio
 // It returns an error when the maximum number of retries is reached.
 func ExponentialBackOff(cwf ChatWorkFunc, initialDelay float64, exponentialBase float64, jitter float64, maxRetries int /*, errors []error*/) func(messages []openai.ChatCompletionMessage, model string) (interface{}, interface{}) {
 	return func(messages []openai.ChatCompletionMessage, model string) (interface{}, interface{}) {
-
-		res, err := cwf.DoChatWork(messages, model)
-		if err == nil {
-			return res, nil
-		}
-
 		delay := initialDelay
-		for i := 0; i < maxRetries; i++ {
-
-			log.Println("ExponentialBackOff Retry: ", i)
-			rand.Seed(time.Now().UnixNano())
-			delay *= exponentialBase * (1.0 + jitter*rand.Float64())
-			log.Println("Failed, sleeping time: ", delay)
-			time.Sleep(time.Duration(delay) * time.Second)
-
-			res, err = cwf.DoChatWork(messages, model)
+		var res interface{}
+		for i := 0; i <= maxRetries; i++ {
+			if i != 0 {
+				log.Println("ExponentialBackOff Retry: ", i)
+				rand.Seed(time.Now().UnixNano())
+				delay *= exponentialBase * (1.0 + jitter*rand.Float64())
+				log.Println("Sleeping time: ", delay)
+				time.Sleep(time.Duration(delay) * time.Second)
+			}
+			// Using an intermediate variable triggers automatic type conversion.
+			// Directly using 'res' to receive the return value of a function will cause a type mismatch.
+			resp, err := cwf.DoChatWork(messages, model)
+			res = resp
 			if err == nil {
 				return res, nil
 			}
